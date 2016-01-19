cff6784fe504d730b7a260626bb06b1ff90ef87c
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-01-19 02:07:42 +0100

    Add index.js
    Export main methods in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    alphabet:       require('./lib/alphabet'),
+    romanize:       require('./lib/romanize'),
+    hangulify:      require('./lib/hangulify'),
+
+    assemble:       require('./lib/assemble'),
+    disassemble:    require('./lib/disassemble'),
+};
